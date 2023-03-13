99c847d270e8f46b84791f8580c9ea2fe988ddb5
Amanda Lima <amandalimasiva@gmail.com>
2023-03-13 20:30:40 -0300

    Adicionando comando Customizado

--- cypress/integration/1-getting-started/aluraPic/login.spec.js
+++ cypress/integration/1-getting-started/aluraPic/login.spec.js
@@ -1,23 +1,18 @@
+
+
 describe("Alura busca cursos", () => {
   beforeEach(() => {
     cy.visit("https://alura-fotos.herokuapp.com/#/home");
   });
 
   it("Realizar login de usuário válido", () => {
-    cy.get("h4[class=text-center]").should("have.text", "Login");
-    cy.get("input[formcontrolname=userName]").type("teste_amanda");
-    cy.get("input[formcontrolname=password]").type("12345678");
-    cy.contains("button", "login").click();
-
+    cy.login('teste_amanda', '12345678')
     cy.contains("a", "(Logout)").click();
     cy.get("h4[class=text-center]").should("have.text", "Login");
   });
 
   it("Realizar login de usuário inválido", () => {
-    cy.get("h4[class=text-center]").should("have.text", "Login");
-    cy.get("input[formcontrolname=userName]").type("teste_aman");
-    cy.get("input[formcontrolname=password]").type("12345678");
-    cy.contains("button", "login").click();
+    cy.login('aman', '1236')
     cy.on("window:alert", (str) => {
       expect(str).to.equal("Invalid user name or password");
     });
---
+++ cypress/support/gui_commands.js
@@ -0,0 +1,6 @@
+Cypress.Commands.add('login', (nome, senha) => {
+    cy.get("h4[class=text-center]").should("have.text", "Login");
+    cy.get("input[formcontrolname=userName]").type(nome);
+    cy.get("input[formcontrolname=password]").type(senha);
+    cy.contains("button", "login").click();
+});
