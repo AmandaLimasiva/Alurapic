c8658900263c037dc8bca7cd1737dee4e64f2a0c
Amanda Lima <amandalimasiva@gmail.com>
2023-03-16 00:03:37 -0300

    CT Login via API

--- cypress/integration/1-getting-started/api-aluraPic/test-api-aluraPic.spec.js
+++ cypress/integration/1-getting-started/api-aluraPic/test-api-aluraPic.spec.js
@@ -12,3 +12,22 @@ describe(' Procurar por fotos e dados via API', () => {
       })
     })
 })
+
+describe('Fazer Login por API', () => {
+  it.only('Buscar minhas fotos', () => {
+    cy.request({
+      method:'POST',
+      url:'https://apialurapic.herokuapp.com/user/login',
+      body: Cypress.env()
+    }).then((res) => {
+      expect(res.status).to.be.equal(200)
+      expect(res.body).is.not.empty
+      expect(res.body).to.have.property('id')
+      expect(res.body.id).to.be.equal(11)
+
+      //email
+      expect(res.body).to.have.property('email')
+      expect(res.body.email).to.be.equal('mands@gmail.com')
+    })
+  })
+})
